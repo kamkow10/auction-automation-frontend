2ff7086f5a46e9868c3a826cc61e72d3dba07618
Kamil Kowalczyk <kamkow1998@gmail.com>
2022-06-05 15:34:36 +0200

    added images

--- src/app/components/offer-tile/offer-tile.component.ts
+++ src/app/components/offer-tile/offer-tile.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Offer} from "../../models/offer";
 
 @Component({
